0dbe52ef3ce245ce5b1fdaf26ed7f09788f90265
Saidul Hofmann <shofmann@hsr.ch>
2017-10-10 00:08:56 +0200

    Javascript added for model and controller.

--- static/js/main.js
+++ static/js/main.js
@@ -9,20 +9,44 @@ Author: Saidul Hofmann
 // Type definitions.
 // ****************************************************************************
 
+class SortDataEnum {
+    static BY_FINISH_DATE(){
+        return "BY_FINISH_DATE";
+    }
+    static BY_CREATE_DATE(){
+        return "BY_CREATE_DATE";
+    }
+    static BY_IMPORTANCE(){
+        return "BY_IMPORTANCE";
+    }
+}
+
 class Note {
     constructor() {
         this.id = createGuid();
         this.title = "";
         this.description = "";
         this.importance = 1;
-        this.completionDate = new Date().toDateString();
-        this.isFinished = false;
+        this.createDate = new Date().toDateString();
+        this.finishDate = new Date().toDateString();
     }
 }
 
 class Model {
     constructor() {
         this.storage = window.localStorage;
+        this.notes = createNotesStorage();
+        this.currentSortOrder = SortDataEnum.BY_IMPORTANCE();
+        this.filterByFinished = false;
+    }
+
+    createNotesStorage(){
+        let notesJsonObj = window.localStorage.getItem("notesJsonObj");
+        if(!notesJsonObj){
+            localStorage.setItem("notesJsonObj", JSON.stringify([]));
+            notesJsonObj = localStorage.getItem("notesJsonObj");
+        }
+        this.notes = JSON.parse(notesJsonObj);
     }
 
     createNote() {
@@ -50,12 +74,82 @@ class Model {
                 notes.push(JSON.parse(this.storage[i]));
             }
         }
+
+        switch(this.currentSortOrder){
+            case SortDataEnum.BY_IMPORTANCE():
+                notes.sort((a,b) => {a.importance - b.importance});
+                break;
+            case SortDataEnum.BY_CREATE_DATE():
+                notes.sort((a,b) => {a.createDate - b.createDate});
+                break;
+            case SortDataEnum.BY_FINISH_DATE():
+                notes.sort((a,b) => {a.finishDate - b.finishDate});
+                break;
+        }
+
         return notes;
     }
 
+    setSortOrderByImportance() {
+        return getAllNotes()
+        this.currentSortOrder = SortDataEnum.BY_IMPORTANCE();
+    }
+    setSortOrderByCreateDate() {
+        return getAllNotes()
+        this.currentSortOrder = SortDataEnum.BY_CREATE_DATE();
+    }
+    setSortOrderByFinishDate() {
+        this.currentSortOrder = SortDataEnum.BY_FINISH_DATE();
+    }
+    toggleFilterByFinished() {
+        if(this.filterByFinished){
+            this.filterByFinished = false;
+        } else {
+            this.filterByFinished = true;
+        }
+    }
     getNotesCount() {
         return this.storage.length;
     }
+}
+
+class Controller {
+    constructor(model) {
+        //Access to Model
+        this.model = model;
+
+        // Access to View
+        this.btnCreateNewNote = null;
+        this.btnSortByFinishDate = null;
+        this.btnSortByCreateDate = null;
+        this.btnSortByImportance = null;
+        this.btnFilterByFinished = null;
+        this.currentStyleSheet = "";
+    }
+
+    bootstrap(){
+        this.btnSortByCreateDate.onclick = (e) => {
+            this.model.setSortOrderByCreateDate();
+            renderUI();
+        }
+        this.btnSortByFinishDate.onclick = (e) => {
+            this.model.setSortOrderByFinishDate();
+            renderUI();
+        }
+        this.btnSortByImportance.onclick = (e) => {
+            this.model.setSortOrderByImportance();
+            renderUI();
+        }
+        this.btnFilterByFinished.onclick = (e) => {
+            this.model.toggleFilterByFinished();
+            renderUI();
+        }
+    }
+
+    renderUI(){
+
+    }
+
 }
 
 
@@ -83,6 +177,7 @@ function addNote(){
 
 
 
+
 // Global variables
 // ****************************************************************************
 
@@ -102,22 +197,22 @@ notes = JSON.parse(notes);
 
 
 
-
-
-
-// Initialisation
+// Actions after the DOM has been fully loaded and parsed.
 // ****************************************************************************
-
-/**
- * Actions after the DOM has been fully loaded and parsed.
- */
 document.addEventListener('DOMContentLoaded', () => {
 
     let model = new Model();
+    let controller = new Controller(model);
+
+    // Bootstrapping
+    controller.btnCreateNewNote = document.getElementById('btnCreateNewNote');
+    controller.btnSortByFinishDate = document.getElementById('btnSortByFinishDate');
+    controller.btnSortByCreateDate = document.getElementById('btnSortByCreateDate');
+    controller.btnSortByImportance = document.getElementById('btnSortByImportance');
+    controller.btnFilterByFinished = document.getElementById('btnFilterByFinished');
 
     // Display notes.
     document.getElementById("notesCount").innerText = notes.length;
     document.getElementById("notes").innerHTML = notes.length == 0 ? "Keine Notizen vorhanden." : notes.join("</br>"); //temporary solution.
-
 
 });
